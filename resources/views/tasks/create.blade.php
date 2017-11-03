8133e22580299620c87efca0e3e2c4f95d87eb19
JunjiYoshihara <yoshihara8238@gmail.com>
2017-11-03 06:05:00 +0000

    tasklist3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,7 +7,8 @@
     <h1>タスクの新規作成ページ</h1>
 
  <div class="row">
-        <div class="col-xs-12 col-sm-ofset-2 col-sm-8 col-md-ofset-2 col-md-8 col-lg-ofset-3 col-lg-6">
+          <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
             
 
             {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
